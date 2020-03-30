0e83b7d91603b2dbd36392e993f93ee4523e7816
Mala Widiyanto <mala.widiyanto@gmail.com>
2020-03-30 07:29:35 +0700

    adding get button WA

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -58,6 +58,22 @@
 
     {!! SettingHelper::gaScripts() !!}
 
+    <!-- Getbutton.io widget -->
+    <script type="text/javascript">
+        (function () {
+            var options = {
+                whatsapp: "+62 852-8480-1818", // WhatsApp number
+                call_to_action: "Contact Us", // Call to action
+                position: "right", // Position may be 'right' or 'left'
+            };
+            var proto = document.location.protocol, host = "getbutton.io", url = proto + "//static." + host;
+            var s = document.createElement('script'); s.type = 'text/javascript'; s.async = true; s.src = url + '/widget-send-button/js/init.js';
+            s.onload = function () { WhWidgetSendButton.init(host, proto, options); };
+            var x = document.getElementsByTagName('script')[0]; x.parentNode.insertBefore(s, x);
+        })();
+    </script>
+    <!-- /Getbutton.io widget -->
+
 </body>
 
 </html>
